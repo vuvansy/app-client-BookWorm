cd72dcfa1b8d8dad199e92e291418d83d3fa4793
TaVanTuan <tatuan290703@gmail.com>
2025-01-18 11:22:01 +0700

    Add login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,9 +3,13 @@ import LoginForm from "./form-login";
 
 const LoginPage = () => {
     return (
-        <div className="">
-            <h1>Trang login</h1>
-            <LoginForm />
+        <div className="bg-bg-main">
+            <div className="container bg-white rounded-lg my-5">
+                <div className="w-[600px] mx-auto py-7">
+                    <div className=" text-heading3  mb-[10px] flex items-center justify-center">Đăng Nhập</div>
+                    <LoginForm />
+                </div>
+            </div>
         </div>
     )
 }
