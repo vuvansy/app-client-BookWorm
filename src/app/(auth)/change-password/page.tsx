3830ec9a1722039d70d402b1b58410de4e5b49ca
TaVanTuan <tatuan290703@gmail.com>
2025-02-12 13:27:36 +0700

    Fix Layout Form Login, Register, Forgot Password, Update Password, Change Password, Profile

--- src/app/(auth)/change-password/page.tsx
+++ src/app/(auth)/change-password/page.tsx
@@ -4,8 +4,8 @@ import ChangePasswordForm from "./form-change-password";
 
 const UpdatePasswordPage = () => {
     return (
-        <div className="bg-bg-main">
-            <div className="container bg-white rounded-lg my-5">
+        <div className="bg-bg-main pt-5">
+            <div className="container bg-white rounded-lg ">
                 <div className="w-[600px] mx-auto py-7">
                     <div className="text-heading3  mb-[10px] flex items-center justify-center">Đổi Mật Khẩu</div>
                     <ChangePasswordForm />
