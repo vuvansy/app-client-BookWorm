fd33ef0f2879e58e60995bc2493fb566fbbdff49
khang <vovankhang184@gmail.com>
2025-03-17 21:38:36 +0700

    Responsive footer

--- src/components/footer/app-footer.tsx
+++ src/components/footer/app-footer.tsx
@@ -17,25 +17,21 @@ const AppFooter = () => {
     <footer className="pt-[30px] bg-bg-main ">
       <div className=" container bg-white flex flex-col lg:flex-row pt-[16px] rounded-lg">
         <div className="relative basis-2/6 text-info ">
-          <div className="w-[389px] h-[69px] mb-[8px] py-2.5 px-[15px]">
-            <Link href={"/"} className='w-[200px]'>
-              <div className="relative w-[200px]">
+          <div className=" w-full h-[69px] mb-[8px] py-2.5 pr-[15px] ">
+              <div className="relative w-[300px] h-[49px] ">
+            <Link href={"/"}>
                 <Image
                   src={"/icon/logo.png"}
-                  alt={'logo BookWorm'}
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{
-                    width: "100%",
-                    height: "auto",
-                  }}
+                  alt="logo BookWorm"
+                  layout="fill"
+                  objectFit="contain"
                   priority
-                  className="w-full object-cover"
+                  className="w-full h-full"
                 />
-              </div>
             </Link>
+              </div>
           </div>
+
           <div className="ml-[10px] mt-[10px] ">
             <p className="pr-[15px] pl-[10px]">
               Lầu 3, 387-389 Hai Bà Trưng, Quận 3, TP. HCM <br />
@@ -43,9 +39,7 @@ const AppFooter = () => {
               60 - 62 Lê Lợi, Quận 1, TP. HCM, Việt Nam
             </p>
             <p className="pt-[5px] pr-[15px] pl-[10px]">
-              Lầu 3, 387-389 Hai Bà Trưng, Quận 3, TP. HCM <br />
-              Công Ty Cổ Phần Phát Hành Sách TP HCM - BookWorm <br />
-              60 - 62 Lê Lợi, Quận 1, TP. HCM, Việt Nam
+            BookWorm nhận đặt hàng trực tuyến và giao hàng tận nơi. KHÔNG hỗ trợ đặt mua và nhận hàng trực tiếp tại văn phòng cũng như tất cả Hệ Thống  BookWorm trên toàn quốc.
             </p>
             <div className="flex mt-[20px] pl-[10px] pb-[15px] md:mt-[10px] ">
               <a className="pr-[10px]" href="#">
@@ -68,8 +62,8 @@ const AppFooter = () => {
           <div className="absolute right-0 top-1/2 transform -translate-y-1/2 h-[95%] border-r border-[#cecece]"></div>
         </div>
         <div className="basis-4/6 ml-[20px] mt-[10px] md:mt-0 md:ml-[10px]">
-          <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 ">
-            <div className="px-[10px] mb-[10px] ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
+            <div className="max-w-[350px] w-full px-[10px] mb-[10px] ">
               <h3 className="text-body-bold  ">DỊCH VỤ</h3>
               <ul className="text-info  mt-[10px] space-y-[5px] md:space-y-[10px] ">
                 <li>
@@ -90,7 +84,7 @@ const AppFooter = () => {
                   >
                     <span className="h-0.5 w-4 bg-[#ea7696] scale-y-0 group-hover:scale-y-100 origin-bottom transition-transform duration-1000 ease-in-out"></span>
                     <span className="inline-block transform translate-x-[-1.5rem] group-hover:translate-x-0 transition-transform duration-1000 ease-in-out text-base md:text-xl lg:text-2xl">
-                      Chính sách bảo mật thông tin cá nhân
+                      Chính sách bảo mật thông tin
                     </span>
                   </a>
                 </li>
@@ -130,7 +124,7 @@ const AppFooter = () => {
                 </li>
               </ul>
             </div>
-            <div className="px-[10px] mb-[10px]">
+            <div className="max-w-[350px] w-full px-[10px] mb-[10px]">
               <h3 className="text-body-bold ">HỖ TRỢ</h3>
               <ul className="text-info  mt-[10px] space-y-[5px] md:space-y-[10px]">
                 <li>
@@ -180,7 +174,7 @@ const AppFooter = () => {
               </ul>
             </div>
 
-            <div className="px-[10px] mb-[10px]">
+            <div className="max-w-[350px] w-full px-[10px] mb-[10px]">
               <h3 className="text-body-bold ">TÀI KHOẢN CỦA TÔI</h3>
               <ul className="text-info  mt-[10px] space-y-[5px] md:space-y-[10px]">
                 <li>
